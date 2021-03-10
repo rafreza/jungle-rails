4f40b5cc60b49be6176cd81ea9e40d36617d5578
Rafin Reza <rafintokyo@gmail.com>
2021-03-10 04:37:27 +0000

    add product details test

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Users can navigate from the home page.", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: "Apparel"
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset("apparel1.jpg"),
+        quantity: 10,
+        price: 64.99,
+      )
+    end
+  end
+
+  scenario "They go to the product detail page by clicking on a product" do
+
+    visit root_path
+  
+    click_link("Details »", match: :first)
+
+    expect(page).to have_css '.product-detail'
+
+
+    save_screenshot('test2.png')
+
+
+  end
+end
